20ef784cabcb14f1962ceb5edf45378b5d6f63e1
xjane <tinklexjane@gmail.com>
2017-08-25 11:46:36 +0800

    27-29

--- 29-todos.rb
+++ 29-todos.rb
@@ -18,16 +18,25 @@ while (true)
   if command == "add"
     print "请输入代办事项: "
     # ...
+    todos << gets
   elsif command == "remove"
     print "请输入要删除的编号: "
     # ...
+    index = gets.to_i
+    todos.delete_at(index)
   elsif command == "save"
     puts "存盘离开"
-
+    File.open('todos.txt', 'w+') do |f|
+      f.truncate(0)
+      todos.each do |t|
+        f << t
+        f << "\n"
+      end
+    end
     # ...
+    show_content(todos)
     break;
   else
     puts "看不懂，请再输入一次"
   end
 end
-
